996398c816e1a5f43acccadfec473edb8cf1e097
Kent Kingery <Kent Kingery>
2025-12-07 17:02:44 -0600

    Added ProductAPI

--- services/productAPI/main.go
+++ services/productAPI/main.go
@@ -3,10 +3,21 @@ package main
 import (
 	"log/slog"
 	"os"
+
+	"github.com/google/uuid"
+
+	"globalco/productAPI/models"
 )
 
 var logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
 func main() {
+	product := models.Product{
+		ID:    uuid.New(),
+		Name:  "Sample Product",
+		Price: 29.99,
+	}
+	logger.Info("Product API service started")
+	logger.Info("Created product", "product", product)
 	logger.Info("Product API service started")
 }
